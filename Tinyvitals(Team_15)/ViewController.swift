be40a0f961581a0fa96120ac03ef684e34768c14
Vineeth Aitha <vineethaitha1234@gmail.com>
2025-11-03 18:41:21 +0530

    Initial Commit

---
+++ Tinyvitals(Team_15)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinyvitals(Team_15)
+//
+//  Created by user70 on 03/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
